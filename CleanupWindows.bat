18509846d4a76db67878b4c377b2f732795228d9
Ace Duke <125328098+Implementation-Tasks@users.noreply.github.com>
2024-02-16 10:55:31 +0700

    Update CleanupWindows.bat
    Signed-off-by: Ace Duke <125328098+Implementation-Tasks@users.noreply.github.com>

--- CleanupWindows.bat
+++ CleanupWindows.bat
@@ -5,11 +5,20 @@ echo.
 :: Remove temporary files
 echo Removing temporary and junk files...
 del /f /s /q %temp%\*.*
-del /f /s /q %systemroot%\Temp\*.*
-del /f /s /q %systemroot%\Prefetch\*.*
-del /f /s /q %systemroot%\Recent Place\*.*
-
+del /f /s /q %prefetch%\*.*
+del /f /s /q %recentplaces%\*.*
 echo.
+
+:: Efficient Junk Removal 
+echo clear the Browser Cache and Old Log Files:
+del /s /q "%LocalAppData%\Google\Chrome\User Data\Default\Cache\*.*"
+del /s /q "C:\Windows\Logs\*.*"
+echo 
+
+:: Registry Cleanup:
+echo 
+reg check "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run"
+echo
 
 :: Clear Recycle Bin
 echo Clearing Recycle Bin...
@@ -26,10 +35,23 @@ echo Cleaning up system files...
 cleanmgr /sageset:1
 cleanmgr /sagerun:1
 echo.
+
+:: RAM Optimization:
+echo
+net stop Superfetch
+net start Superfetch
+echo
+
 :: Clear up your RAM Cache
 echo Cleaning up RAM Cache...
 ipconfig /FlushDNS
 echo.
+:: Privacy Protection
+echo Clear browser history (for Google Chrome)..
+del /s /q "%LocalAppData%\Google\Chrome\User Data\Default\History"
+Remove cookies (for Internet Explorer):
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
+echo
 :: Defragment hard drive
 echo Defragmenting hard drive...
 defrag C: /U /V
